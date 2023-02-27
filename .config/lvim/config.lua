81fa2ea18ed22341112561ce306763dc38719c12
Chang-Yen Tseng <10328757+Summon528@users.noreply.github.com>
2023-02-26 21:26:12 -0600

    auto format

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -11,7 +11,7 @@ vim.opt.relativenumber = false
 lvim.log.level = "info"
 lvim.format_on_save = {
   enabled = true,
-  pattern = "*.lua",
+  pattern = "*",
   timeout = 1000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
